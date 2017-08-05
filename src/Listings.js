c04b7e23c25101dfbe0a70a70bbbe4f3028019fa
OUWANG <wangou@bc.edu>
2017-08-04 19:52:06 -0700

    gif rendering

--- src/Listings.js
+++ src/Listings.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-
 class Listings extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        y: 0,
+        x: ''
+      }
+  }
 
   helper() {
     axios.get('http://api.giphy.com/v1/gifs/trending?limit=25&api_key=82c148f30bec4898a39c457eb6d3dc13')
-    .then(function(response) {
-      console.log(response.data.data[0].url);
+    .then((response) => {
+      console.log(response.data.data[0].embed_url);
+      return response.data.data[0].embed_url;
+    })
+    .then((res) => {
+      console.log(res);
+      this.setState({
+        y: 1,
+        x: res
+      })
     })
     .catch(function(error) {
       console.log(error);
@@ -21,7 +35,13 @@ class Listings extends Component {
         <button onClick={()=>this.helper()}>DATA FROM Giphy</button>
         <p className="App-intro">
           Listing Section
+          {this.state.y}
         </p>
+        <p> Gif:{this.state.x}
+        </p>
+          <div>This is GIF IMAGE:
+            <embed src={this.state.x}/>
+          </div>
       </div>
     );
   }
